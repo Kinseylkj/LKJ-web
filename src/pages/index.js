565d8a3034fb2e91fcaa9a833f8bca4b17d1b2bd
刘凯精 <liukaijing@liukaijingdeMacBook-Pro.local>
2018-10-01 10:53:50 +0800

    v0.1

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>HELLO WORD</h1>
+    <p>这是我的第一个网站</p>
+    <p>我还没想好要怎么做</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
