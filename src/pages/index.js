8bcaa9c9797dd91274351fb071918032f7deb380
刘凯精 <liukaijing@liukaijingdeMacBook-Pro.local>
2018-10-01 18:07:17 +0800

    v0.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,9 @@ const IndexPage = () => (
     <div className="Hero">
      <div className="HeroGroup">
        <h1>HELLOWORD</h1>
-       <p>这是我的第一个网站</p>
-       <Link to="/page-2/">Go to page 2</Link>
+       <p>这是我的第一个网站你喜欢吗？
+       </p>
+       <Link to="/page-2/">向我表白</Link>
      </div>
     </div>
   </div>
