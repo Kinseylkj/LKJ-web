3459ec921b4e55356c49dac9a4392d8d02c6d650
刘凯精 <liukaijing@liukaijingdeMacBook-Pro.local>
2018-10-04 19:07:42 +0800

    v0.6

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-     <Link to="/"><img src={require('../images/logo.svg')}  alt="logo" width="34" /></Link>
+     <Link to="/"><img src={require('../images/logo.svg')}  alt="logo" height="30" /></Link>
      <Link to="/home">Home</Link>
      <Link to="/design">Design</Link>
      <Link to="/about me">About Me</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ const IndexPage = () => (
     <div className="Hero">
      <div className="HeroGroup">
        <h1>HELLOWORD</h1>
-       <p>这是我的第一个网站你喜欢吗？
+       <p>很高兴能够与你分享我的设计、见解和看法。<br />但行好事，勿问前程。
        </p>
-       <Link to="/page-2/">向我表白</Link>
+       <Link to="/page-2/"><button data-ripple>MY DESIGN</button></Link>
      </div>
     </div>
   </Layout>
