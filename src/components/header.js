835f9d2eef8b6ebd9079a8d65bf98b1efbd561ac
刘凯精 <liukaijing@liukaijingdeMacBook-Pro.local>
2018-10-06 18:13:39 +0800

    有了动效

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-     <Link to="/"><img src={require('../images/logo.svg')}  alt="logo" height="30" /></Link>
-     <Link to="/home">Home</Link>
-     <Link to="/design">Design</Link>
-     <Link to="/about me">About Me</Link>
+     <Link to="/"><img src={require('../images/logo.svg')}  alt="logo" height="34" /></Link>
+     <Link to="/home">首页</Link>
+     <Link to="/design">我的设计</Link>
+     <Link to="/about me">关于我</Link>
     </div> 
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,11 @@ const IndexPage = () => (
        <h1>HELLOWORD</h1>
        <p>很高兴能够与你分享我的设计、见解和看法。<br />但行好事，勿问前程。
        </p>
-       <Link to="/page-2/"><button data-ripple>MY DESIGN</button></Link>
+       <div className="HeroLink">
+       <Link to="/page-2/"><button>我的设计</button></Link>
+       <Link to="/about me">关于我</Link>
+     </div>
+     
      </div>
     </div>
   </Layout>
